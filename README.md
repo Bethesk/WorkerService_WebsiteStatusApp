10f602bb00b5fe449d9471ce05c3d730edb3ec58
Shin <skang@sos.ca.gov>
2022-05-20 10:05:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WorkerService_WebsiteStatusApp
+ 
---
+++ WorkerService_WebsiteStatus/Worker.cs
@@ -0,0 +1,33 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WorkerService_WebsiteStatus
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private HttpClient client;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
+                //Await one second
+                await Task.Delay(5000, stoppingToken);
+            }
+        }
+    }
+}
